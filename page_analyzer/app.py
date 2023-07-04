af52a3c954071da951031835a381bedbc5b1ccc7
Evgeny Sokolov <sokolovsolod@gmail.com>
2023-07-04 20:35:43 +0700

    v 0.2.2 Add files for deploying
    Add build.sh, requirements.txt

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,7 @@
 from flask import (
-    Flask, request, render_template, redirect,
-    url_for, flash, get_flashed_messages, session
+    Flask
+    # request, render_template, redirect,
+    # url_for, flash, get_flashed_messages, session
 )
 import os
 from dotenv import load_dotenv
