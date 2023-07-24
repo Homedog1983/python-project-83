c03c66c508b10401aae77f189a8ad7efad223bab
Evgeny Sokolov <sokolovsolod@gmail.com>
2023-07-24 16:59:21 +0700

    v 0.3.11 Delete from __init__ app-importing (pytest error was raised)

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,6 +1,6 @@
-from page_analyzer.app import app as page_analyzer
+# from page_analyzer.app import app as page_analyzer
 
 
-__all__ = [
-    'page_analyzer',
-]
+# __all__ = [
+#     'page_analyzer',
+# ]
